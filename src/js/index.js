b20eefe96ec5bb4e7f4adbcde93e3f400608f43c
Yourockit <sts2ro@gmail.com>
2023-08-21 09:14:20 +0300

    -add-footer-popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,22 @@
-//LANG-DROPBOX
+//LANG-DROPBOX-CONST
 const langButton = document.querySelector(".lang-selector__lang-button");
 const navbarDropdown = document.querySelector(".navbar-lang-dropdown");
 const closeButton = document.querySelector(".lang-selector__close-button");
 
+//FOOTER-MENU-DROPBOX-CONST
+const menuItems = document.querySelectorAll("[data-item]");
+const submenus = document.querySelectorAll("[data-submenu]");
+
+//FOOTER-POPUP-CONST
+const btnOtherLang = document.querySelector(
+  ".navbar-lang-dropdown__other-language"
+);
+const footerPopup = document.querySelector(".footer-popup");
+const btnCloseFooterPopup = document.querySelector(
+  ".footer-popup__close-button"
+);
+
+//LANG-DROPBOX
 langButton.addEventListener("click", function () {
   navbarDropdown.classList.remove("disabled");
 });
@@ -12,10 +26,6 @@ closeButton.addEventListener("click", function () {
 });
 
 //FOOTER-MENU-DROPBOX
-
-const menuItems = document.querySelectorAll("[data-item]");
-const submenus = document.querySelectorAll("[data-submenu]");
-
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
     submenus.forEach((submenu, submenuIndex) => {
@@ -26,4 +36,14 @@ menuItems.forEach((item, index) => {
       }
     });
   });
+});
+
+//FOOTER-POPUP
+btnOtherLang.addEventListener("click", () => {
+  footerPopup.classList.remove("disabled");
+  navbarDropdown.classList.add("disabled");
+});
+
+btnCloseFooterPopup.addEventListener("click", () => {
+  footerPopup.classList.add("disabled");
 });
